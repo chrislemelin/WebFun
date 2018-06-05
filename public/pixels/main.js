99b9a47b5faa9ee48606ab2ec40f745c1e91702b
Christopher Lemelin <clemelin2@gmail.com>
2018-06-05 15:15:29 -0400

    added a button to go to a random linkeroni

--- public/pixels/main.js
+++ public/pixels/main.js
@@ -16,242 +16,9 @@ const RANDOM_CURATED_LINKS=['Bn7giN9', 'hIfYTfs', 'A4HN4zB', '0sPlzS9', 'BSEaAlt
     'sHjLAYE', 'zZ8QJLf', 'ICHetCo', 'CWRAkgx', 'MP75LeW', 'CUHEsZQ', '2Lm80Bj',
     'yrtguvP', '71bnHwN']
 const BASE_URL = 'https://chrislemelin.gitlab.io/WebFun/pixels/'
+const DEFAULT_ID = 'MGphLLc';
+const IMGUR_API_KEY = '2ab48d8265ee7ba';
 
-
-class World{
-
-    constructor(backgroundColor = new Color(255,255,255))
-    {
-        this.backgroundColor = backgroundColor;
-        this.pixels = [];
-    }
-
-    draw()
-    {
-        ctx.beginPath();
-        ctx.fillStyle = this.backgroundColor.toString();
-        ctx.rect(0, 0, canvas.width, canvas.height );
-        ctx.fill();
-
-        var date = new Date()
-        this.pixels.forEach(
-            (pixel) => pixel.draw(date)
-        )
-
-        myRequestAnimationFrame(()=>this.draw());
-
-    }
-
-    addPixel(pixel)
-    {
-        this.pixels.push(pixel);
-    }
-
-    handleMouseMove(x,y)
-    {
-        var date = new Date()
-        this.pixels.forEach(
-            (pixel, index, object) => {if (pixel.shoudSplit(x,y,date)){
-                var newPixels = pixel.split()
-                object.push(... newPixels);
-
-
-                object.splice(index, 1);
-            }}
-        )
-        //this.draw();
-    }
-
-}
-
-class Pixel{
-    constructor(newX, newY, newLength, oldPixel, newColor = undefined, animationLength = 500)
-    {
-        this.x = newX;
-        this.y = newY;
-        //this.color = newColor;
-        this.length = newLength;
-        this.creationTime = new Date();
-        this.oldPixel = oldPixel;
-        this.animationLength = animationLength
-        if(newColor == undefined)
-            this.color = getColorFromPicture(this.x, this.y, this.length, this.length);
-        else
-            this.color = newColor;
-    }
-
-    draw(currentTime)
-    {
-        var animationTween = (currentTime - this.creationTime);
-
-        var x = this.x;
-        var y = this.y;
-        var length = this.length;
-        var color = this.color;
-
-        if(animationTween < this.animationLength && this.oldPixel != undefined)
-        {
-            var ratio = animationTween/ this.animationLength
-            x = tween(this.oldPixel.x , x, ratio);
-            y = tween(this.oldPixel.y , y, ratio);
-            length = tween(this.oldPixel.length , length, ratio);
-            color = this.color.tween(this.oldPixel.color, ratio);
-
-        }
-        ctx.beginPath();
-        ctx.fillStyle = color.toString();
-        ctx.strokeStyle= "rbg(0,0,0)"
-        ctx.rect(x, y, length, length);
-        ctx.fill();
-
-    }
-
-    split()
-    {
-        var gap = 0;
-        var newPixels = [];
-        var startLength = this.length/2;
-        var endLength = startLength;
-        if(endLength > 0)
-        {
-            // upper left
-            var transitionPixel = new Pixel(this.x, this.y, startLength, undefined, this.color)
-            newPixels.push(new Pixel(this.x, this.y, endLength, transitionPixel));
-
-            // upper right
-            transitionPixel = new Pixel(this.x + startLength, this.y, startLength, undefined, this.color);
-            newPixels.push(new Pixel(this.x + startLength+gap, this.y, endLength, transitionPixel));
-
-            // lower left
-            transitionPixel = new Pixel(this.x, this.y + startLength, startLength, undefined, this.color)
-            newPixels.push(new Pixel(this.x, this.y+ startLength+ gap, endLength, transitionPixel));
-
-            // // lower right
-            transitionPixel = new Pixel(this.x + startLength, this.y + startLength, startLength, undefined, this.color)
-            newPixels.push(new Pixel(this.x + startLength + gap, this.y + startLength + gap, endLength, transitionPixel));
-        }
-
-        return newPixels;
-    }
-
-    shoudSplit(x,y, currentTime){
-        var animationTween = (currentTime - this.creationTime);
-
-        return (x > this.x && x < (this.x+this.length) &&
-           y > this.y && y < this.y+this.length &&
-           !(animationTween < this.animationLength && this.oldPixel != undefined) &&
-           this.length/2 > MIN_PIXEL_LENGTH );
-    }
-
-}
-
-class Color{
-    constructor(newR, newG, newB, newA = 255)
-    {
-        this.r = newR;
-        this.g = newG;
-        this.b = newB;
-        this.a = newA;
-    }
-
-    toString()
-    {
-        var returnString = 'rgba('+ Math.round(this.r) + ', ' + Math.round(this.g)
-         + ', ' + Math.round(this.b) + ' ,'+ Math.round(this.a) +')';
-        return returnString;
-    }
-
-    randomColor(range)
-    {
-        var col =  new Color(
-            (this.r + Math.random()*range - range/2).clamp(0,255),
-            (this.g + Math.random()*range - range/2).clamp(0,255),
-            (this.b + Math.random()*range - range/2).clamp(0,255)
-        )
-        return col;
-    }
-
-    tween(oldColor, ratio)
-    {
-        return new Color(
-            tween(oldColor.r, this.r, ratio),
-            tween(oldColor.g, this.g, ratio),
-            tween(oldColor.b, this.b, ratio)
-        )
-    }
-}
-
-/// graphics helper functions
-function tween(num1, num2, ratio)
-{
-    var difference = num2 - num1;
-    return (ratio * difference) + num1;
-}
-
-function randomColor()
-{
-    return new Color(Math.random()*255,Math.random()*255,Math.random()*255);
-}
-
-function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
-
-function getColorFromPicture(x, y, xLength, yLength)
-{
-    //return new Color(255,0,0);
-    numberOfPixels = xLength * yLength;
-    var r = 0, g = 0, b = 0;
-    var data = pictureCtx.getImageData(x,y,xLength, yLength).data;
-    var counter = 0;
-    for(var a = 0; a < numberOfPixels; a++)
-    {
-
-        var i = a*4;
-        if(data[i] != undefined)
-            r+=data[i];
-        if(data[i+1] != undefined)
-            g += data[i+1];
-        if(data[i+2] != undefined)
-            b += data[i+2];
-        if(data[i] != undefined &&
-            data[i+1] != undefined &&
-            data[i+2] != undefined)
-            counter++;
-
-
-
-    }
-    r /= counter;
-    b /= counter;
-    g /= counter;
-
-    var returnColor = new Color(r, g, b);
-    if(counter === 0 )
-    {
-        returnColor = new Color(255,255,255);
-    }
-
-    return returnColor;
-
-}
-
-function initCanvas() {
-    canvas  = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
-    width = canvas.height= imageWidth;
-    height = canvas.width = canvas.style.height =imageHeight;
-    canvas.style.width =imageWidth+"px";
-    canvas.style.height = imageHeight+"px";
-
-
-    world = new World();
-    world.addPixel(new Pixel(0, 0 , imageWidth));
-
-    world.draw();
-
-}
 
 function initPicture()
 {
@@ -264,7 +31,7 @@ function initPicture()
     }
     else {
         //default
-        loadCanvas('https://i.imgur.com/MGphLLc.jpg');
+        loadCanvas('https://i.imgur.com/'+DEFAULT_ID+'.png');
     }
 }
 
@@ -273,7 +40,6 @@ function loadCanvas(url)
     var pictureCanvas = document.getElementById('picture');
     pictureCtx = pictureCanvas.getContext('2d');
 
-
     var img = new Image();
     img.crossOrigin = "Anonymous";
 
@@ -281,28 +47,21 @@ function loadCanvas(url)
     //drawing of the test image - img1
     img.onload = function () {
 
-        imageWidth = Math.min(img.width, MAX_PIXELS);
-        imageHeight = Math.min(img.height, MAX_PIXELS);
+        resizeAndDraw(pictureCanvas, pictureCtx, img)
 
-        imageWidth = Math.max(imageWidth, imageHeight);
-        imageHeight = imageWidth;
-
-        pictureCanvas.width = imageWidth;
-        pictureCanvas.height = imageHeight;
-        //draw background image
+        imageWidth = pictureCanvas.width;
+        imageHeight = pictureCanvas.height;
 
         var compositeOperation = pictureCtx.globalCompositeOperation;
 
-
-
-        pictureCtx.drawImage(img, 0, 0);
+        //pictureCtx.drawImage(img, 0, 0, imageWidth, imageHeight);
 
 
         //set to draw behind current content
         pictureCtx.globalCompositeOperation = "destination-over";
 
         pictureCtx.fillStyle = "rgb(255,255,255)";
-        pictureCtx.fillRect(0, 0, imageWidth, imageHeight );
+        pictureCtx.fillRect(0, 0, pictureCanvas.width, pictureCanvas.height );
 
 
 
@@ -318,9 +77,7 @@ function loadCanvas(url)
 
 
 function imgurUpload(data) {
-    var applicationId = '2ab48d8265ee7ba';
-    var auth = 'Client-ID ' + applicationId;
-
+    var auth = 'Client-ID ' + IMGUR_API_KEY;
 
     $.ajax({
       url: 'https://api.imgur.com/3/image',
@@ -345,61 +102,56 @@ function imgurUpload(data) {
     });
 }
 
-function getResizedImage()
+function resizedImage(canvas, ctx)
 {
-    if(localStorage.dataBase64 != "")
+    if(loadedImage != "")
     {
         var loadPictureCanvas = document.getElementById('loadPicture');
         var loadPictureCtx = loadPictureCanvas.getContext('2d');
 
         var image = new Image();
         image.onload = function() {
-
-            var resizedWidth = image.width;
-            var resizedHeight = image.height;
-
-            if(resizedWidth > resizedHeight)
-            {
-                if(resizedWidth > MAX_PIXELS)
-                {
-                    mult = MAX_PIXELS/resizedWidth;
-                    resizedWidth = MAX_PIXELS;
-                    resizedHeight = resizedHeight * mult;
-                }
-            }
-            else {
-                if(resizedHeight > MAX_PIXELS)
-                {
-                    mult = MAX_PIXELS/resizedHeight;
-                    resizedHeight = MAX_PIXELS;
-                    resizedWidth = resizedWidth * mult;
-                }
-
-            }
-
-            loadPictureCanvas.width = resizedWidth;
-            loadPictureCanvas.height = resizedHeight;
-
-            loadPictureCtx.drawImage(image, 0, 0, resizedWidth, resizedHeight);
+            resizeAndDraw(loadPictureCanvas, loadPictureCtx, image)
             imgurUpload(loadPictureCanvas.toDataURL("image/png"))
         };
-        image.src = localStorage.dataBase64;
+        image.src = loadedImage;
 
 
 
     }
     return "";
 }
+function resizeAndDraw(canvas, ctx, image)
+{
+    var resizedWidth = image.width;
+    var resizedHeight = image.height;
 
+    if(resizedWidth > resizedHeight)
+    {
+        mult = MAX_PIXELS/resizedWidth;
+        resizedWidth = MAX_PIXELS;
+        resizedHeight = resizedHeight * mult;
+
+    }
+    else {
+        mult = MAX_PIXELS/resizedHeight;
+        resizedHeight = MAX_PIXELS;
+        resizedWidth = resizedWidth * mult;
+    }
+    canvas.width = Math.max(resizedWidth, resizedHeight);
+    canvas.height = Math.max(resizedWidth, resizedHeight);
+    ctx.drawImage(image, 0, 0, resizedWidth, resizedHeight);
+
+}
 
 function imageIsLoaded(e) {
 
     $('#myImg').attr('src', e.target.result);
-    localStorage.dataBase64 = e.target.result;
+    loadedImage = e.target.result;
 };
 
 
-function initCallbacks()
+function initPage()
 {
     $(":file").change(function () {
 
@@ -410,15 +162,12 @@ function initCallbacks()
                reader.readAsDataURL(this.files[0]);
            }
        });
-    $("#send").click(getResizedImage);
+    $("#send").click(resizedImage);
     $("#randomCurated").click(goToRandomCuratedLink);
+    $("#randomLink").click(goToRealRandomLink);
     document.body.addEventListener("mousemove",handleMouseMove);
     document.body.addEventListener("touchstart", handleTouchMove, false);
     document.body.addEventListener("touchmove", handleTouchMove, false);
-
-
-
-
 }
 
 
@@ -450,15 +199,33 @@ function goToRandomCuratedLink()
 
 function goToRealRandomLink()
 {
+    var auth = 'Client-ID ' + IMGUR_API_KEY;
 
+    $.ajax({
+      url: 'https://api.imgur.com/3/gallery/random/random/',
+      type: 'GET',
+      headers: {
+        Authorization: auth,
+        Accept: 'application/json'
+    },
+      success: function(result)
+      {
+          var randomID = result.data[0].id;
+          window.location.href = BASE_URL+"?id="+randomID;
+        },
+      error: function(result) {
+          console.log("error: "+result)
+      }
+
+    });
 }
 
 
 function init() {
 
     initPicture();
-    initCallbacks();
-    localStorage.dataBase64="";
+    initPage();
+    loadedImage="";
 }
 
 window.onload = init;
---
+++ public/pixels/pixelsCanvas.js
@@ -0,0 +1,235 @@
+
+class World{
+
+    constructor(backgroundColor = new Color(255,255,255))
+    {
+        this.backgroundColor = backgroundColor;
+        this.pixels = [];
+    }
+
+    draw()
+    {
+        ctx.beginPath();
+        ctx.fillStyle = this.backgroundColor.toString();
+        ctx.rect(0, 0, canvas.width, canvas.height );
+        ctx.fill();
+
+        var date = new Date()
+        this.pixels.forEach(
+            (pixel) => pixel.draw(date)
+        )
+
+        myRequestAnimationFrame(()=>this.draw());
+
+    }
+
+    addPixel(pixel)
+    {
+        this.pixels.push(pixel);
+    }
+
+    handleMouseMove(x,y)
+    {
+        var date = new Date()
+        this.pixels.forEach(
+            (pixel, index, object) => {if (pixel.shoudSplit(x,y,date)){
+                var newPixels = pixel.split()
+                object.push(... newPixels);
+
+
+                object.splice(index, 1);
+            }}
+        )
+        //this.draw();
+    }
+
+}
+
+class Pixel{
+    constructor(newX, newY, newLength, oldPixel, newColor = undefined, animationLength = 500)
+    {
+        this.x = newX;
+        this.y = newY;
+        //this.color = newColor;
+        this.length = newLength;
+        this.creationTime = new Date();
+        this.oldPixel = oldPixel;
+        this.animationLength = animationLength
+        if(newColor == undefined)
+            this.color = getColorFromPicture(this.x, this.y, this.length, this.length);
+        else
+            this.color = newColor;
+    }
+
+    draw(currentTime)
+    {
+        var animationTween = (currentTime - this.creationTime);
+
+        var x = this.x;
+        var y = this.y;
+        var length = this.length;
+        var color = this.color;
+
+        if(animationTween < this.animationLength && this.oldPixel != undefined)
+        {
+            var ratio = animationTween/ this.animationLength
+            x = tween(this.oldPixel.x , x, ratio);
+            y = tween(this.oldPixel.y , y, ratio);
+            length = tween(this.oldPixel.length , length, ratio);
+            color = this.color.tween(this.oldPixel.color, ratio);
+
+        }
+        ctx.beginPath();
+        ctx.fillStyle = color.toString();
+        ctx.strokeStyle= "rbg(0,0,0)"
+        ctx.rect(x, y, length, length);
+        ctx.fill();
+
+    }
+
+    split()
+    {
+        var gap = 0;
+        var newPixels = [];
+        var startLength = this.length/2;
+        var endLength = startLength;
+        if(endLength > 0)
+        {
+            // upper left
+            var transitionPixel = new Pixel(this.x, this.y, startLength, undefined, this.color)
+            newPixels.push(new Pixel(this.x, this.y, endLength, transitionPixel));
+
+            // upper right
+            transitionPixel = new Pixel(this.x + startLength, this.y, startLength, undefined, this.color);
+            newPixels.push(new Pixel(this.x + startLength+gap, this.y, endLength, transitionPixel));
+
+            // lower left
+            transitionPixel = new Pixel(this.x, this.y + startLength, startLength, undefined, this.color)
+            newPixels.push(new Pixel(this.x, this.y+ startLength+ gap, endLength, transitionPixel));
+
+            // // lower right
+            transitionPixel = new Pixel(this.x + startLength, this.y + startLength, startLength, undefined, this.color)
+            newPixels.push(new Pixel(this.x + startLength + gap, this.y + startLength + gap, endLength, transitionPixel));
+        }
+
+        return newPixels;
+    }
+
+    shoudSplit(x,y, currentTime){
+        var animationTween = (currentTime - this.creationTime);
+
+        return (x > this.x && x < (this.x+this.length) &&
+           y > this.y && y < this.y+this.length &&
+           !(animationTween < this.animationLength && this.oldPixel != undefined) &&
+           this.length/2 > MIN_PIXEL_LENGTH );
+    }
+
+}
+
+class Color{
+    constructor(newR, newG, newB, newA = 255)
+    {
+        this.r = newR;
+        this.g = newG;
+        this.b = newB;
+        this.a = newA;
+    }
+
+    toString()
+    {
+        var returnString = 'rgba('+ Math.round(this.r) + ', ' + Math.round(this.g)
+         + ', ' + Math.round(this.b) + ' ,'+ Math.round(this.a) +')';
+        return returnString;
+    }
+
+    randomColor(range)
+    {
+        var col =  new Color(
+            (this.r + Math.random()*range - range/2).clamp(0,255),
+            (this.g + Math.random()*range - range/2).clamp(0,255),
+            (this.b + Math.random()*range - range/2).clamp(0,255)
+        )
+        return col;
+    }
+
+    tween(oldColor, ratio)
+    {
+        return new Color(
+            tween(oldColor.r, this.r, ratio),
+            tween(oldColor.g, this.g, ratio),
+            tween(oldColor.b, this.b, ratio)
+        )
+    }
+}
+
+/// graphics helper functions
+function tween(num1, num2, ratio)
+{
+    var difference = num2 - num1;
+    return (ratio * difference) + num1;
+}
+
+function randomColor()
+{
+    return new Color(Math.random()*255,Math.random()*255,Math.random()*255);
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function getColorFromPicture(x, y, xLength, yLength)
+{
+    //return new Color(255,0,0);
+    numberOfPixels = xLength * yLength;
+    var r = 0, g = 0, b = 0;
+    var data = pictureCtx.getImageData(x,y,xLength, yLength).data;
+    var counter = 0;
+    for(var a = 0; a < numberOfPixels; a++)
+    {
+
+        var i = a*4;
+        if(data[i] != undefined)
+            r+=data[i];
+        if(data[i+1] != undefined)
+            g += data[i+1];
+        if(data[i+2] != undefined)
+            b += data[i+2];
+        if(data[i] != undefined &&
+            data[i+1] != undefined &&
+            data[i+2] != undefined)
+            counter++;
+
+
+
+    }
+    r /= counter;
+    b /= counter;
+    g /= counter;
+
+    var returnColor = new Color(r, g, b);
+    if(counter === 0 )
+    {
+        returnColor = new Color(255,255,255);
+    }
+
+    return returnColor;
+
+}
+
+function initCanvas() {
+    canvas  = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    width = canvas.height= imageWidth;
+    height = canvas.width = canvas.style.height =imageHeight;
+    canvas.style.width =imageWidth+"px";
+    canvas.style.height = imageHeight+"px";
+
+
+    world = new World();
+    world.addPixel(new Pixel(0, 0 , imageWidth));
+
+    world.draw();
+
+}
